bfddd5a16ab75d0eb43bdbb4cedf5b2bf5c8d285
juanjhidalgor <juanjhidalgor1@gmail.com>
2024-12-25 11:44:31 -0500

    start project-info view

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { ProjectComponent } from './pages/project/project.component';
+import { HomeComponent } from './pages/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: ProjectComponent},
+    {path: 'home', component: HomeComponent}
+];
